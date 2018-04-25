3b0a6051154b85bea1cde64994cef7bff24aa25a
Kmilo135 <38697749+Kmilo135@users.noreply.github.com>
2018-04-24 23:36:20 -0300

    Pruebas Varias

--- Web/Administrador/WebVerUsuarios.aspx.cs
+++ Web/Administrador/WebVerUsuarios.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using Modelo;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -38,15 +39,7 @@ namespace Web.Administrador
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            Service1 service = new Service1();
-            string datos = service.ListarUsuarios();
-            XmlSerializer ser = new XmlSerializer(typeof(Modelo.UsuarioCollection));
-            StringReader reader = new StringReader(datos);
-            
-            Modelo.UsuarioCollection listaUsuario = (Modelo.UsuarioCollection)ser.Deserialize(reader);
-            reader.Close();
-            gvUsuario.DataSource = listaUsuario;
-            gvUsuario.DataBind();
+            CargarGriedView();
         }
 
         //Creación de Sesión
@@ -66,42 +59,77 @@ namespace Web.Administrador
             }
         }
 
-        protected void gvUsuario__RowEditing(object sender, EventArgs e)
-        {
-            Response.Redirect("../Administrador/WebEditarUsuario.aspx");
-        }
-        /*
-        private void CargarTablaDetalle() {//Carga pestañas por linea
-            PtoControl control = new PtoControl();
-            List<Recorrido> collections = RecorridoCollections.ReadAll();
-            //Linea 1
-            Recorrido recorrido = collections.FirstOrDefault
-                (
-                    x => x.Nombre.Contains("1")
-                );
-            List<PtoControlRecorrido> ptos = PtoControlRecorridoCollections.ReadRuta(recorrido.Id).OrderBy(x => x.Sentido).ThenBy(x => x.Numero).ToList();
-            BoundField newColumnName = new BoundField();
 
-            newColumnName.DataField = "Maquina";
-            newColumnName.HeaderText = "Máquina";
+        
+        private void CargarGriedView() {
+            //Carga listas con datos
+            List<Usuario> users = UsuarioCollection.ListaUsuarios();
+            List<Modelo.Empleado> empleados = EmpleadoCollection.ListaEmpleados();
+            List<Modelo.Cliente> clientes = ClienteCollection.ListaClientes();
+            List<Modelo.Proveedor> proveedores = ProveedorCollection.ListaProveedores();
 
-            gvDetalles.Columns.Add(newColumnName);
+            Usuario user;
+            
+            //Creacion DataTable
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[5] {
+                new DataColumn("ID", typeof(int)),
+                new DataColumn("Nombre", typeof(string)),
+                new DataColumn("Usuario",typeof(string)),
+                new DataColumn("Tipo",typeof(string)),
+                new DataColumn("Estado",typeof(string))
+            });
 
-            newColumnName.DataField = "Maquina";
-            newColumnName.HeaderText = "Máquina";
-
-            gvDetalles.Columns.Add(newColumnName);
-            foreach (PtoControlRecorrido p in ptos) {
-                control.Id = p.IdControl;
-                control.Read();
-                newColumnName.DataField = p.Id.ToString() + "-" + p.Sentido + "-" + p.Numero;
-                newColumnName.HeaderText = control.Nombre;
-
-                gvDetalles.Columns.Add(newColumnName);
+            //Carga de datos en DataTable
+            foreach (Modelo.Cliente c in clientes) {
+                user = new Usuario();
+                var list = users.Where(x => x.ID_USUARIO == c.ID_USUARIO).ToList();
+                foreach (var item in list) {
+                    user.NOMBRE_USUARIO = item.NOMBRE_USUARIO;
+                    user.TIPO_USUARIO = item.TIPO_USUARIO;
+                    user.ESTADO = item.ESTADO;
+                }
+                dt.Rows.Add(c.ID_USUARIO,c.NOMBRE_CLIENTE,user.NOMBRE_USUARIO,user.TIPO_USUARIO,user.ESTADO);
             }
 
-            gvDetalles.DataSource = New List(Of String)
+            foreach (Modelo.Empleado e in empleados) {
+                user = new Usuario();
+                var list = users.Where(x => x.ID_USUARIO == e.ID_USUARIO).ToList();
+                foreach (var item in list) {
+                    user.NOMBRE_USUARIO = item.NOMBRE_USUARIO;
+                    user.TIPO_USUARIO = item.TIPO_USUARIO;
+                    user.ESTADO = item.ESTADO;
+                }
+                dt.Rows.Add(e.ID_USUARIO,e.PNOMBRE_EMPLEADO+ " " + e.APP_PATERNO_EMPLEADO + " " + e.APP_MATERNO_EMPLEADO,user.NOMBRE_USUARIO,user.TIPO_USUARIO,user.ESTADO);
+            }
+
+            foreach (Modelo.Proveedor p in proveedores) {
+                user = new Usuario();
+                var list = users.Where(x => x.ID_USUARIO == p.ID_USUARIO).ToList();
+                foreach (var item in list) {
+                    user.NOMBRE_USUARIO = item.NOMBRE_USUARIO;
+                    user.TIPO_USUARIO = item.TIPO_USUARIO;
+                    user.ESTADO = item.ESTADO;
+                }
+                dt.Rows.Add(p.ID_USUARIO,p.PNOMBRE_PROVEEDOR + " " + p.APP_PATERNO_PROVEEDOR + " " + p.APP_MATERNO_PROVEEDOR,user.NOMBRE_USUARIO,user.TIPO_USUARIO,user.ESTADO);
+            }
+
+            //Carga de GriedView
+            gvUsuario.DataSource = dt;
+            gvUsuario.DataBind();
         }
-        */
+
+        protected void gvUsuario_RowEditing(object sender,GridViewEditEventArgs e) {
+            string tipo = gvUsuario.DataKeys[e.NewEditIndex].Value.ToString();
+            if (tipo.Equals(Tipo_Usuario.Administrador.ToString()) || tipo.Equals(Tipo_Usuario.Empleado.ToString())) {
+                Response.Redirect("../Empleado/WebEditarEmpleado.aspx");
+            }
+            if (tipo.Equals(Tipo_Usuario.Administrador.ToString()) || tipo.Equals(Tipo_Usuario.Empleado.ToString())) {
+                Response.Redirect("../Cliente/WebEditarCliente.aspx");
+            }
+            if (tipo.Equals(Tipo_Usuario.Administrador.ToString()) || tipo.Equals(Tipo_Usuario.Empleado.ToString())) {
+                Response.Redirect("../Proveedor/WebEditarProveedor.aspx");
+            }
+        }
     }
 }
